c07daf8d1970e9fb723d40cf186c06b1782d8e73
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-24 14:33:29 -0500

    Working with lambda functions

--- Python_Entry/helloworld.py
+++ Python_Entry/helloworld.py
@@ -1,4 +1,6 @@
-text1 = "hello"
-text2 = "world"
-print(text1, text2)
-print("the end", "or is it", "keep watching to learn about python", 456, 354)
+# text1 = "hello"
+# text2 = "world"
+# print(text1, text2)
+# print("the end", "or is it", "keep watching to learn about python", 456, 354)
+
+print((lambda x, z: x + z) (10, 20))
