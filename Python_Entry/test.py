82a88c82f99b326d9e4dd8e451910dc7827be579
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-25 22:45:28 -0500

    Error handling in python

--- Python_Entry/test.py
+++ Python_Entry/test.py
@@ -168,11 +168,38 @@
 # #4 Output all of the cats singing using the my_pets instance
 # my_pets.walk()
 
-my_student = {
-    "name": "Rolf Smith",
-    "grades": [70, 88, 90, 99]
-}
+# my_student = {
+#     "name": "Rolf Smith",
+#     "grades": [70, 88, 90, 99]
+# }
 
-def average(student):
-    return sum(student["grades"]) / len(student["grades"])
-print(average(my_student))
+# def average(student):
+#     return sum(student["grades"]) / len(student["grades"])
+# print(average(my_student))
+
+
+
+# def power_of_two():
+#     user_input = input('Please enter a number: ')
+#     try:
+#         n = float(user_input)
+#     except ValueError:
+#         print('Your input was invalid.')
+#     finally:
+#         n_square = n ** 2
+#         return n_square
+# print(power_of_two())
+
+def interact():
+    while True:
+        try:
+            user_input = int(input('Please input an integer:'))     # try to turn user input into an integer
+        except ValueError:
+            print('Please input integers only.')  # print a message if user didn't input an integer
+        else:
+            print('{} is {}.'.format(user_input, 'even' if user_input % 2 == 0 else 'odd'))     # print even/odd if the user input an integer
+        finally:    # regardless of the previous input being valid or not
+            user_input = input('Do you want to play again? (y/N):')     # ask if the user wants to play again
+            if user_input != 'y':       # quit if the user didn't input `y`
+                print('Goodbye.')
+                break   # break the while loop to quit
